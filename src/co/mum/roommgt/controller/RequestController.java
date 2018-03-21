b47ff4f3be28539d580bacd2c052d594a16b5b3c
joseph <jktusoy@gmail.com>
2018-03-21 16:15:43 -0500

    update in webxml

--- src/co/mum/roommgt/controller/RequestController.java
+++ src/co/mum/roommgt/controller/RequestController.java
@@ -1,11 +1,23 @@
 package co.mum.roommgt.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import co.mum.roommgt.dao.student.StudentDAO;
+import co.mum.roommgt.model.Account;
+import co.mum.roommgt.model.Student;
 
 /**
  * Servlet implementation class RequestController
@@ -26,8 +38,25 @@ public class RequestController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/json");
+		response.setCharacterEncoding("UTF-8");
+
+		PrintWriter out = response.getWriter();
+		 
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		 
+
+		String transType = request.getParameter("transType");
+	
+		// decided to use gson instead of jackson, gson is much lighter
+		if (transType.toLowerCase().equals("add")) {
+		 
+		} 
+		System.out.print(transType);
+		 
+		out.println(transType);
+		out.flush();
+		out.close();
 	}
 
 	/**
